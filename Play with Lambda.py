7e5558979c1fb3d27cf5a605834d4e0f0b30ba27
Samar Bhojane <64828955+charulingah@users.noreply.github.com>
2022-04-03 11:38:41 +0530

    Create Play with Lambda.py

---
+++ Play with Lambda.py
@@ -0,0 +1,8 @@
+'''Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.'''
+#Solution
+
+(lambda a,num : a+int(input("num_to_add"))) (25, num)
+
+'''sample input: 10 
+sample output: 35'''
+
